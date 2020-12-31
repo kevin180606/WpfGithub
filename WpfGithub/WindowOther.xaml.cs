b3eb34cb98c27023b02cb154f9ce0f122260b835
goufuhua <kevin.gou@hotmail.com>
2020-12-31 13:48:37 +0800

    add a new windows

--- WpfGithub/WindowOther.xaml.cs
+++ WpfGithub/WindowOther.xaml.cs
@@ -10,26 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfGithub
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// WindowOther.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WindowOther : Window
     {
-        public MainWindow()
+        public WindowOther()
         {
             InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("buyi");
-
-
+            MessageBox.Show("second win");
         }
     }
 }
